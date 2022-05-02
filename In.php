f3e2ac7172d52dd7f014c665597dc4bccc91fdc2
Kush Patel <47339310+kmoney99@users.noreply.github.com>
2022-05-02 16:46:01 -0400

    Update In.php

--- In.php
+++ In.php
@@ -1,12 +1,16 @@
-<div class="container">
-		<h1>Posts</h1>
-		<?php foreach($posts as $post) : ?>
-			<div class="well">
-				<h3><?php echo $post['title']; ?></h3>
-				<small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
-				<p><?php echo $post['body']; ?></p>
-				<a class="btn btn-default" href="<?php echo ROOT_URL; ?>post.php?id=<?php echo $post['id']; ?>">Read More</a>
-			</div>
-		<?php endforeach; ?>
-	</div>
-	<link rel="stylesheet" type="text/css" href="http://bootswatch.com/cerulean/bootstrap.min.css">
+<div class="container"> 
+<link rel="stylesheet" type="text/css" href="http://bootswatch.com/cerulean/bootstrap.min.css">
+        <h1>Forum Post</h1>
+        
+<?php foreach($forums as $forum) : ?>
+
+        <div class="well">
+                <h3><?php echo $post['question']; ?></h3>
+                <small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['name']; ?></small>
+                <p><?php echo $post['discussion']; ?></p>
+        </div>
+
+<?php endforeach; ?>
+
+</div>
+
